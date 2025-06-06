0e7d45ce8dab3b08b947c067e87b446cd69be7f8
l <eh.chahkiev@smartics.ru>
2025-06-06 23:01:53 +0300

    feat: create room

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router'
 
 export const routes: Routes = [
-
+  { path: '', loadComponent: () => import('./create-invite/create-invite').then(m => m.CreateInviteComponent) },
   { path: 'invite/:uid', loadComponent: () => import('./prepare-invite/prepare-invite').then(m => m.PrepareInviteComponent) },
 ]
--- src/app/create-invite/create-invite.ts
+++ src/app/create-invite/create-invite.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { Clipboard } from '@angular/cdk/clipboard'
+import { uid } from 'radashi'
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-create-invite',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './create-invite.html',
   styleUrl: './create-invite.css'
 })
-export class CreateInvite {
+export class CreateInviteComponent {
+  constructor(
+    private clipboard: Clipboard,
+  ) {}
 
+  private snackbar = inject(MatSnackBar)
+  private inviteLink?: string
+
+  protected createInviteLink(): string {
+    if (!this.inviteLink) {
+      const origin = window.location.origin
+      this.inviteLink = `${origin}/invite/${uid(12)}`
+    }
+    if (this.clipboard.copy(this.inviteLink)) {
+      this.snackbar.open(`Ссылка ${this.inviteLink} скопирована`, 'Закрыть', {
+        duration: 3000,
+      });
+    }
+    return this.inviteLink
+  }
 }
