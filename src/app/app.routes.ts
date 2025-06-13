da5ae19b0783a7b5c950b39144dc8f777ded0281
l <eh.chahkiev@smartics.ru>
2025-06-13 22:11:37 +0300

    feat: lobby page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router'
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./create-room/create-room').then(m => m.CreateRoomComponent),
+    loadComponent: () => import('./lobby/lobby').then(m => m.LobbyComponent),
   },
   {
     title: 'Room',
--- src/app/lobby/lobby.ts
+++ src/app/lobby/lobby.ts
@@ -3,33 +3,15 @@ import { Component, signal } from '@angular/core'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 
 @Component({
   selector: 'app-lobby',
-  imports: [MatDividerModule, MatButtonModule, MatIconModule],
+  imports: [MatDividerModule, MatButtonModule, MatIconModule, MatListModule],
   templateUrl: './lobby.html',
   styleUrl: './lobby.css'
 })
-export class Lobby {
+export class LobbyComponent {
 
-  isMicrophoneEnabled = signal(false)
-  isHeadphonesEnabled = signal(false)
-
-  private microphoneStream: MediaStream | null = null
-
-  toggleHeadphones() {
-    this.isHeadphonesEnabled.set(!this.isHeadphonesEnabled())
-  }
-
-  async toggleMicrophone() {
-    this.microphoneStream = await navigator.mediaDevices.getUserMedia({ audio: true })
-    this.isMicrophoneEnabled.set(!this.isMicrophoneEnabled())
-    if (this.isMicrophoneEnabled()) {
-     return
-    } else if (this.microphoneStream) {
-      this.microphoneStream.getAudioTracks().forEach(track => track.stop())
-      this.microphoneStream.getTracks().forEach(track => track.stop())
-    }
-  }
 
 }
